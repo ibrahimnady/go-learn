3ef77caf41499e6f96a18fc50d5d5cfa42473948
ibrahimnady <ibrahimnady5@gmail.com>
2025-01-19 21:31:17 +0200

    remove error

--- app/Intro/page.jsx
+++ app/Intro/page.jsx
@@ -16,7 +16,7 @@ import imageIntro from '/public/homePage.webp';
 import IMGintro1 from '/public/abwaab-scholarship.svg';
 import IMGintro2 from '/public/images.jpg';
 import IMGintro3 from '/public/helpTeacher.jpg';
-// import Subscription from '../Subscription/page.jsx';
+
 
 
 
@@ -316,50 +316,7 @@ function Intro() {
             اشحن محفظتك الان
           </Typography>
           {/* <Subscription /> */}
-          <Box
-                          sx={{
-                              backgroundColor: '#1976d2',
-                              padding: 4,
-                              borderRadius: 4,
-                              color: 'white',
-                              textAlign: 'center',
-                              maxWidth: 600,
-                              margin: '0 auto',
-                              mt: 4
-                          }}
-                      >
-                          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 3 }}>
-                              {/* <img src="https://via.placeholder.com/300" alt="Student" style={{ width: 100, height: 100 }} /> */}
-                          </Box>
           
-                          <Typography variant="h4" component="h2" sx={{ mb: 2, fontWeight: 'bold' }}>
-                              كن مستعد للتفوق في دراستك!
-                          </Typography>
-          
-                          <Typography variant="h6" sx={{ mb: 4 }}>
-                              تعلم وارفع معدّلك من خلال فيديوهات تشرح موادك الدراسية واختبارات قوية مع أفضل المعلمين
-                          </Typography>
-          
-                          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                              <Button
-                                  variant="contained"
-                                  size="large"
-                                  sx={{
-                                      backgroundColor: theme.palette.background.paper,
-                                      color: theme.palette.text.primary,
-                                      '&:hover': {
-                                          backgroundColor: theme.palette.primary.main,
-                                          color: '#fff'
-                                      },
-                                      fontSize: '1.1rem',
-                                      py: 1.5
-                                  }}
-                              // startIcon={<span style={{ marginLeft: 8 }}>🔑</span>}
-                              >
-                                  اشحن محفظتك الان
-                              </Button>
-                          </Box>
-                      </Box>
         </Box>
       </Container>
     </div >
